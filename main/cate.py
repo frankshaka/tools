1b820849f7539f47092e9a1c6261477acd969627
Frank Shaka <frankshaka@gmail.com>
2013-10-08 18:14:25 +0800

    Add -t parameter to 'cate' command to trim line contents.

--- main/cate.py
+++ main/cate.py
@@ -3,14 +3,22 @@
 
 import sys
 
-def cat(filepath):
+def cat(filepath, trimmed):
     with open(filepath, "r") as source_file:
         for line in source_file:
-            print repr(line)
+            if trimmed:
+                print repr(line)[1:-1]
+            else:
+                print repr(line)
 
 def main():
+    trimmed = False
     for filepath in sys.argv[1:]:
-        cat(filepath)
+        if filepath == "-t":
+            trimmed = True
+        else:
+            cat(filepath, trimmed)
+            trimmed = False
 
 if __name__ == "__main__":
     main()
